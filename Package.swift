e8aa238d7d06b0555904751216d806ba16158710
primetimer <primetimertime@gmail.com>
2019-07-14 10:15:01 +0200

    package

--- Package.swift
+++ Package.swift
@@ -8,17 +8,17 @@ let package = Package(
     products: [
         .library(name: "TILApp", targets: ["App"]),
     ],
+    targets: [
+        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
+    ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
 
            .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc")
-    ],
-    targets: [
-        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
-        .target(name: "Run", dependencies: ["App"]),
-        .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
 
